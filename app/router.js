c3b5b6be0c5e560b01530a72745a0213d0e8eaa4
weijq <772981712@qq.com>
2018-06-06 22:39:50 +0800

    router

--- app/router.js
+++ app/router.js
@@ -4,5 +4,7 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  require('./router/api')(app);
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
+  router.get('/news', controller.news.list);
 };
