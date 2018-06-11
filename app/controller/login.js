843131562df21ca9006c1d9e07d8ca824618be07
weijq <772981712@qq.com>
2018-06-11 22:32:46 +0800

    controller return data format

--- app/controller/login.js
+++ app/controller/login.js
@@ -6,6 +6,9 @@ class LoginController extends Controller {
   async post() {
       console.log(this.ctx.request.body,666666);
     // this.ctx.body = 'hi, egg';
+    const {ctx} =this;
+    ctx.body = { ret:1,userData:[{username:'admin'}] };
+    ctx.status = 201;
   }
 }
 
